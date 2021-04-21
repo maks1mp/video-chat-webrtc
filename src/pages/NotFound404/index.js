e39002e003f4794248d57b2b7fa86a977da70bed
maksympohribniak <mpohribniak@emergemarket.com>
2021-04-21 08:31:33 +0300

    first commit

---
+++ src/pages/NotFound404/index.js
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <div>
+      Main Page
+    </div>
+  );
+}
---
+++ src/pages/Room/index.js
@@ -0,0 +1,7 @@
+export default function NotFound404() {
+  return (
+    <div>
+      NOT FOUND.
+    </div>
+  );
+}
